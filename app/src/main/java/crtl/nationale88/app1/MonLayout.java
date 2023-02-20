83012a218be60a177de3f6f4c9d3caaf6551cfb9
Milo CARTAL <105420069+milocartal@users.noreply.github.com>
2023-02-20 11:15:01 +0100

    menus

--- app/src/main/java/crtl/nationale88/app1/MonLayout.java
+++ app/src/main/java/crtl/nationale88/app1/MonLayout.java
@@ -9,31 +9,54 @@ import android.widget.TextView;
 public class MonLayout extends LinearLayout implements View.OnClickListener {
 
     Button btn;
+    Button btn2;
 
     TextView txt;
 
     int cpt;
+
+    final int BTN_PLUS = 0;
+    final int BTN_MOIN = 1;
+
+    int min = 65;
+    int max = 125;
 
     public MonLayout(Context context) {
         super(context);
         setOrientation(VERTICAL);
 
         btn = new Button(context);
+        btn2 = new Button(context);
         txt = new TextView(context);
-        cpt = 0;
+        cpt = 65;
 
         btn.setText("+");
-        txt.setText("0");
+        btn2.setText("-");
+
+        btn.setId(BTN_PLUS);
+        btn2.setId(BTN_MOIN);
+        txt.setText(Integer.toString(cpt));
 
         btn.setOnClickListener(this);
+        btn2.setOnClickListener(this);
 
         addView(btn);
         addView(txt);
+        addView(btn2);
     }
 
     @Override
     public void onClick(View view) {
-        cpt++;
+        switch(view.getId()){
+            case 0:
+                if (cpt<max)
+                    cpt++;
+                break;
+            case 1:
+                if (cpt>min)
+                    cpt--;
+                break;
+        }
         txt.setText(Integer.toString(cpt));
     }
 }
